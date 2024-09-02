c72dacdde449bfca0130f86ada6a0801e2fae867
David Hardy <hard2737@gmail.com>
2024-09-02 03:19:41 -0600

    fix

--- src/app/features/Vendors/VendorsProvider.tsx
+++ src/app/features/Vendors/VendorsProvider.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React from 'react'
-import VendorList from './VendorList.tsx'
-import VendorInputBar from './VendorInputBar.tsx'
+import VendorList from './VendorList'
+import VendorInputBar from './VendorInputBar'
 import { Provider } from 'react-redux'
 import store from '@/app/lib/store'
 
